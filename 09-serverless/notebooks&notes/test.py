4fce74d88159be0c0a637cd97271a5f93f50cbd6
rui pinto <ruifspinto@gmail.com>
2024-12-08 18:42:31 +0000

    finished serveless homework for mlzoomcamp

--- 09-serverless/notebooks&notes/test.py
+++ 09-serverless/notebooks&notes/test.py
@@ -1,8 +1,14 @@
 import requests
+from pprint import pprint
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+# this is to run locally
+#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+
+
+# this the url of the deployed lambda function in api gateway
+url = 'https://"somethinghere".execute-api.eu-west-2.amazonaws.com/testing/predict'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
 result = requests.post(url, json=data).json()
-print(result)
+pprint(result)
